a9bb8d3100c7580ca2db8dc1f809069fc05b0cbf
veeweeherman <vy.hoang.cu@gmail.com>
2015-07-30 11:48:59 -0700

    refactoring to mongo

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,50 @@
-var db = require('../config');
+// var db = require('../config');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+// create a schema
+var linkSchema = new Schema({
+  url: { type: String, required: true },
+  base_url: { type: String },
+  code:  { type: String },
+  title: { type: String },
+  visits:  { type: Number },
+  created_at: Date,
+  updated_at: Date
 });
 
+// assign default values
+var Link = mongoose.model('Link', userSchema);
+
+// give methods
+linkSchema.methods.initialize = function () {  
+  this.on('creating', function(model, attrs, options){
+    var shasum = crypto.createHash('sha1');
+    shasum.update(model.get('url'));
+    model.set('code', shasum.digest('hex').slice(0, 5));
+  });
+};
+
 module.exports = Link;
+
+
+
+
+/** BEFORE MONGO ****************************/
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,67 @@
-var db = require('../config');
+// var db = require('../config');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
+// create a schema
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: Date,
+  updated_at: Date
+});
+
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+userSchema.methods.initialize = function () {
     this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
+    this.save(function(err) {
+      if (err) {throw err;} 
+      else {console.log('User saved successfully!';}
+    });
+};
+//TODO can it use 'get?' here?
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
       callback(isMatch);
     });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});
+};
 
+userSchema.methods.hashPassword = function(){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
+    });
+};
+
+// make this available to our users in our Node applications
 module.exports = User;
+
+
+
+
+/** BEFORE MONGO ****************************/
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function(){
+//     this.on('creating', this.hashPassword);
+//   },
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function(){
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
+
+// module.exports = User;
